b79407415bbbd9ca5f62309873fd6580c20038ee
adrck <54768877+adrck@users.noreply.github.com>
2026-07-08 18:35:25 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
+async function loadPage(page) {
 
+    const response = await fetch(`pages/${page}.html`);
+
+    const html = await response.text();
+
+    document.getElementById("content").innerHTML = html;
+
+}
+
+window.onload = function () {
+
+    loadPage("home");
+
+};
